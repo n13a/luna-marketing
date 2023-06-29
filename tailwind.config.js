0aea5a8da5587e16fe607ca5c6710e3acb724b92
Nima <nima97e@gmail.com>
2023-06-29 20:46:43 +0330

    fix carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
   theme: {
     extend: {},
   },
